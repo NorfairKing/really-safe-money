b8425bd34c6b31538dfd5489ee7aa5ce020ede03
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-11-21 10:30:35 +0100

    enable cachix in ci

--- flake.nix
+++ flake.nix
@@ -3,124 +3,124 @@
 
   nixConfig = {
     extra-substituters = "https://really-safe-money.cachix.org";
-    extra-trusted-public-keys = "really-safe-money.cachix.org-1:9KkQoGbquHhTEA7SRc/6u97i7ymYo/vamPU1NF28qag="
-      };
+    extra-trusted-public-keys = "really-safe-money.cachix.org-1:9KkQoGbquHhTEA7SRc/6u97i7ymYo/vamPU1NF28qag=";
+  };
 
-    inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
-      nixpkgs-22_11.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
-      nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
-      nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
-      horizon-advance.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-advance";
-      pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-      validity.url = "github:NorfairKing/validity";
-      validity.flake = false;
-      autodocodec.url = "github:NorfairKing/autodocodec";
-      autodocodec.flake = false;
-      safe-coloured-text.url = "github:NorfairKing/safe-coloured-text";
-      safe-coloured-text.flake = false;
-      fast-myers-diff.url = "github:NorfairKing/fast-myers-diff";
-      fast-myers-diff.flake = false;
-      sydtest.url = "github:NorfairKing/sydtest";
-      sydtest.flake = false;
-      dekking.url = "github:NorfairKing/dekking";
-      dekking.flake = false;
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
+    nixpkgs-22_11.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
+    nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
+    nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
+    horizon-advance.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-advance";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    validity.url = "github:NorfairKing/validity";
+    validity.flake = false;
+    autodocodec.url = "github:NorfairKing/autodocodec";
+    autodocodec.flake = false;
+    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text";
+    safe-coloured-text.flake = false;
+    fast-myers-diff.url = "github:NorfairKing/fast-myers-diff";
+    fast-myers-diff.flake = false;
+    sydtest.url = "github:NorfairKing/sydtest";
+    sydtest.flake = false;
+    dekking.url = "github:NorfairKing/dekking";
+    dekking.flake = false;
+  };
 
-    outputs =
-      { self
-      , nixpkgs
-      , nixpkgs-22_11
-      , nixpkgs-22_05
-      , nixpkgs-21_11
-      , horizon-advance
-      , pre-commit-hooks
-      , validity
-      , safe-coloured-text
-      , fast-myers-diff
-      , sydtest
-      , autodocodec
-      , dekking
-      }:
-      let
-        system = "x86_64-linux";
-        nixpkgsFor = nixpkgs: import nixpkgs { inherit system; config.allowUnfree = true; };
-        pkgs = nixpkgsFor nixpkgs;
-        allOverrides = pkgs.lib.composeManyExtensions [
-          (pkgs.callPackage (validity + "/nix/overrides.nix") { })
-          (pkgs.callPackage (fast-myers-diff + "/nix/overrides.nix") { })
-          (pkgs.callPackage (autodocodec + "/nix/overrides.nix") { })
-          (pkgs.callPackage (safe-coloured-text + "/nix/overrides.nix") { })
-          (pkgs.callPackage (sydtest + "/nix/overrides.nix") { })
-          (pkgs.callPackage (dekking + "/nix/overrides.nix") { })
-          self.overrides.${system}
-        ];
-        horizonPkgs = horizon-advance.legacyPackages.${system}.extend allOverrides;
-        haskellPackagesFor = nixpkgs: (nixpkgsFor nixpkgs).haskellPackages.extend allOverrides;
-        haskellPackages = haskellPackagesFor nixpkgs;
-      in
-      {
-        overrides.${system} = pkgs.callPackage ./nix/overrides.nix { };
-        overlays.${system} = import ./nix/overlay.nix;
-        packages.${system} = haskellPackages.reallySafeMoneyPackages;
-        checks.${system} =
-          let
-            backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).reallySafeMoneyRelease;
-            allNixpkgs = {
-              inherit
-                nixpkgs-22_11
-                nixpkgs-22_05
-                nixpkgs-21_11;
-            };
-            backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
-          in
-          backwardCompatibilityChecks // {
-            forwardCompatibility = horizonPkgs.reallySafeMoneyRelease;
-            shell = self.devShells.${system}.default;
-            coverage-report = haskellPackages.dekking.makeCoverageReport {
-              name = "test-coverage-report";
-              packages = [
-                "really-safe-money"
-                "really-safe-money-autodocodec"
-              ];
-              coverage = [
-                "really-safe-money-gen"
-              ];
-            };
-            pre-commit = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                hlint.enable = true;
-                hpack.enable = true;
-                ormolu.enable = true;
-                nixpkgs-fmt.enable = true;
-                nixpkgs-fmt.excludes = [ ".*/default.nix" ];
-                cabal2nix.enable = true;
-              };
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-22_11
+    , nixpkgs-22_05
+    , nixpkgs-21_11
+    , horizon-advance
+    , pre-commit-hooks
+    , validity
+    , safe-coloured-text
+    , fast-myers-diff
+    , sydtest
+    , autodocodec
+    , dekking
+    }:
+    let
+      system = "x86_64-linux";
+      nixpkgsFor = nixpkgs: import nixpkgs { inherit system; config.allowUnfree = true; };
+      pkgs = nixpkgsFor nixpkgs;
+      allOverrides = pkgs.lib.composeManyExtensions [
+        (pkgs.callPackage (validity + "/nix/overrides.nix") { })
+        (pkgs.callPackage (fast-myers-diff + "/nix/overrides.nix") { })
+        (pkgs.callPackage (autodocodec + "/nix/overrides.nix") { })
+        (pkgs.callPackage (safe-coloured-text + "/nix/overrides.nix") { })
+        (pkgs.callPackage (sydtest + "/nix/overrides.nix") { })
+        (pkgs.callPackage (dekking + "/nix/overrides.nix") { })
+        self.overrides.${system}
+      ];
+      horizonPkgs = horizon-advance.legacyPackages.${system}.extend allOverrides;
+      haskellPackagesFor = nixpkgs: (nixpkgsFor nixpkgs).haskellPackages.extend allOverrides;
+      haskellPackages = haskellPackagesFor nixpkgs;
+    in
+    {
+      overrides.${system} = pkgs.callPackage ./nix/overrides.nix { };
+      overlays.${system} = import ./nix/overlay.nix;
+      packages.${system} = haskellPackages.reallySafeMoneyPackages;
+      checks.${system} =
+        let
+          backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).reallySafeMoneyRelease;
+          allNixpkgs = {
+            inherit
+              nixpkgs-22_11
+              nixpkgs-22_05
+              nixpkgs-21_11;
+          };
+          backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
+        in
+        backwardCompatibilityChecks // {
+          forwardCompatibility = horizonPkgs.reallySafeMoneyRelease;
+          shell = self.devShells.${system}.default;
+          coverage-report = haskellPackages.dekking.makeCoverageReport {
+            name = "test-coverage-report";
+            packages = [
+              "really-safe-money"
+              "really-safe-money-autodocodec"
+            ];
+            coverage = [
+              "really-safe-money-gen"
+            ];
+          };
+          pre-commit = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              hlint.enable = true;
+              hpack.enable = true;
+              ormolu.enable = true;
+              nixpkgs-fmt.enable = true;
+              nixpkgs-fmt.excludes = [ ".*/default.nix" ];
+              cabal2nix.enable = true;
             };
           };
-        devShells.${system}.default = haskellPackages.shellFor {
-          name = "really-safe-money-shell";
-          packages = p: builtins.attrValues p.reallySafeMoneyPackages;
-          withHoogle = true;
-          doBenchmark = true;
-          buildInputs = (with pkgs; [
-            zlib
-            cabal-install
-          ]) ++ (with pre-commit-hooks.packages.${system};
-            [
-              hlint
-              hpack
-              nixpkgs-fmt
-              ormolu
-              cabal2nix
-            ]);
-          shellHook = self.checks.${system}.pre-commit.shellHook;
         };
-        nix-ci.cachix = {
-          name = "really-safe-money";
-          public-key = "really-safe-money.cachix.org-1:9KkQoGbquHhTEA7SRc/6u97i7ymYo/vamPU1NF28qag="
-            };
-        };
+      devShells.${system}.default = haskellPackages.shellFor {
+        name = "really-safe-money-shell";
+        packages = p: builtins.attrValues p.reallySafeMoneyPackages;
+        withHoogle = true;
+        doBenchmark = true;
+        buildInputs = (with pkgs; [
+          zlib
+          cabal-install
+        ]) ++ (with pre-commit-hooks.packages.${system};
+          [
+            hlint
+            hpack
+            nixpkgs-fmt
+            ormolu
+            cabal2nix
+          ]);
+        shellHook = self.checks.${system}.pre-commit.shellHook;
+      };
+      nix-ci.cachix = {
+        name = "really-safe-money";
+        public-key = "really-safe-money.cachix.org-1:9KkQoGbquHhTEA7SRc/6u97i7ymYo/vamPU1NF28qag=";
+      };
+    };
 
-      }
+}
