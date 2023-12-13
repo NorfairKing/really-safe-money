df1a04a7059ab6599a005db5f4c3b74538be8c88
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-12-13 11:14:18 +0100

    faster conversion rate generator

--- really-safe-money-gen/src/Money/ConversionRate/Gen.hs
+++ really-safe-money-gen/src/Money/ConversionRate/Gen.hs
@@ -3,6 +3,9 @@
 module Money.ConversionRate.Gen where
 
 import Data.GenValidity
+import Data.Ratio
 import Money.ConversionRate
 
-instance GenValid ConversionRate
+instance GenValid ConversionRate where
+  genValid = ConversionRate <$> ((%) <$> (succ <$> genValid) <*> (succ <$> genValid))
+  shrinkValid = fmap ConversionRate . filter (== 0) . shrinkValid . unConversionRate
