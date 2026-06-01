2ab6de95330336c3c9fdab31a6cc95ce428ca532
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-06-01 14:07:50 +0200

    Upgrade to lts-24.38 + nixpkgs 26.05; gate doctest behind cabal flag
    Add a `doctest` cabal flag (default: true) to really-safe-money and
    really-safe-money-autodocodec, with the doctest test-suites marked
    `buildable: !flag(doctest)`. Disable the flag in stack.yaml so
    `stack test` no longer fails on `Could not find module 'Autodocodec'`
    under stack, where doctest's `-isrc` load path doesn't see transitive
    dependencies. Nix builds keep the flag on and continue to run both
    doctests.
    
    Note in stack.yaml that `stack test` still fails on lts-24.38 under the
    nix devShell because of a stack 3.7.1 + nix-ghc-with-packages
    interaction around attoparsec's internal sublib unit-id when configuring
    yaml. `nix flake check` is the working feedback loop on this branch.

--- flake.nix
+++ flake.nix
@@ -7,10 +7,8 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
-    nixpkgs-25_05.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
-    nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
-    nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-26.05";
+    nixpkgs-25_11.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
     horizon-advance.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-advance";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     validity.url = "github:NorfairKing/validity";
@@ -32,9 +30,7 @@
   outputs =
     { self
     , nixpkgs
-    , nixpkgs-25_05
-    , nixpkgs-24_11
-    , nixpkgs-24_05
+    , nixpkgs-25_11
     , horizon-advance
     , pre-commit-hooks
     , validity
@@ -72,8 +68,7 @@
           backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).reallySafeMoneyRelease;
           allNixpkgs = {
             inherit
-              nixpkgs-24_11
-              nixpkgs-24_05;
+              nixpkgs-25_11;
           };
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
